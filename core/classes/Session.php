d7cf5e92711cc24f1efdfe6d6065fe88d630f635
EvgeniyLapinN <EvgeniyyLapin@gmail.com>
2019-03-15 12:14:30 +0700

    added session class

---
+++ core/classes/Session.php
@@ -0,0 +1,36 @@
+<?php  
+
+namespace core\classes;
+
+class Session
+{
+	/**
+	 * [init description]
+	 * @return [type] [description]
+	 */
+	public static function init()
+	{
+		session_start();
+	}
+
+
+	public static function set($key, $value)
+	{
+		$_SESSION[$key] = $value;
+	}
+
+	public static function get($key)
+	{
+		if(isset($_SESSION[$key])) {
+			return $_SESSION[$key];
+		}
+	}
+
+
+	public static function destroy()
+	{
+		session_destroy();
+	}
+}
+
+?>
