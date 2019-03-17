1acdb5090c3c445082087559a8c720a8d4bd14ec
EvgeniyLapin <EvgeniyyLapin@gmail.com>
2019-03-17 22:58:59 +0700

    edited template

--- template/navigation.php
+++ template/navigation.php
@@ -1,9 +1,17 @@
 <nav>
 	<div class="container">
 		<div class="row">
-			<div class="col-md-12">
+			<div class="col-md-10">
+				<ul>
+					<li><a href="/">Главная</a></li>
+					<li><a href="/catalog">Каталог</a></li>
+					<li><a href="/cart">Корзина</a></li>
+				</ul>
+			</div>
+			<div class="col-md-2">
 				<?php 
 					if($this->loggedUser) {
+						echo '<a href="/user/settings">Настройки</a>';
 						echo '<a href="/logout">Выход</a>';
 					} 
 					else {
