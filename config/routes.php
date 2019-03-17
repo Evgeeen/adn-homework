67b811985e43b183cd793d6a4180f2fda9702072
EvgeniyLapin <EvgeniyyLapin@gmail.com>
2019-03-17 22:51:05 +0700

    added routes for user activation method and order method

--- config/routes.php
+++ config/routes.php
@@ -5,13 +5,16 @@ return array(
 	'cart/quantity' => 'cart/quantity',
 	'cart/clear' => 'cart/clear',
 	'cart/remove' => 'cart/removeItem',
-	
 	'cart$' => 'cart/index',
-	'cart/order' => 'cart/order',
+	'order$' => 'order/index',
+
 	'catalog$' => 'catalog/index',
 
 	'auth$' => 'user/login',
 	'logout$' => 'user/logout',
+	'user/activate/([A-Za-z0-9&]+)' => 'user/activate/$1',
+	'user/settings' => 'user/settings',
+	'registration/success' => 'user/registrationSuccess',
 	'registration$' => 'user/registration',
 	'admin/users/page-([0-9]+)' => 'admin/users/$1',
 	'admin/users$' => 'admin/users',
