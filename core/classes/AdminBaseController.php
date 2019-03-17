dfd467d4373fec8fd27e60caacd0f510f6778881
EvgeniyLapin <EvgeniyyLapin@gmail.com>
2019-03-17 23:03:39 +0700

    edited AdminBaseController

--- core/classes/AdminBaseController.php
+++ core/classes/AdminBaseController.php
@@ -5,13 +5,14 @@ namespace core\classes;
 use core\classes\Session;
 
 
-class AdminBaseController {
+class AdminBaseController extends Controller{
 	
 	public $status;
 
 	public function __construct()
 	{
-		Session::init();
+		//Session::init();
+		parent::__construct();
 
 		if(Session::get('type') !== 'admin'){
 			Session::destroy();
