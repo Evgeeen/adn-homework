0f129e5a2deae6078ab593a79428bedfeedd4a0b
EvgeniyLapin <EvgeniyyLapin@gmail.com>
2019-03-18 23:22:50 +0700

    add new order method

--- models/Order.php
+++ models/Order.php
@@ -7,7 +7,14 @@ use core\classes\{Model, File};
 
 class Order extends Model
 {
-
+	public function addOrder($username, $products)
+	{
+		$query_stmt = $this->db->prepare("
+			INSERT INTO orders (`products`, `user_id`)
+			VALUES (:username, :products)");
+		$result = $query_stmt->execute(array('username' => $username, 'products' => $products));
+		return $result;
+	}
 }
 
 ?>
